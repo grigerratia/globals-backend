ef7dba6019401995ec51e5750f2d0a03dea96e8a
Griger Ratia <grigerratia@gmail.com>
2026-09-17 03:37:26 -0400

    fix: Optimize puppeteer args to prevent timeout

--- index.js
+++ index.js
@@ -210,24 +210,17 @@ app.get('/health', (_req, res) => {
 
 const client = new Client({
   authStrategy: new LocalAuth(),
+  authTimeoutMs: 120000,
   puppeteer: {
+    headless: true,
     executablePath: resolveChromePath(),
+    timeout: 120000,
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
       '--disable-dev-shm-usage',
-      '--disable-gpu',
-      '--single-process',
-      '--no-zygote',
-      '--disable-extensions',
-      '--disable-background-networking',
-      '--disable-default-apps',
-      '--disable-sync',
-      '--disable-translate',
-      '--no-first-run',
-      '--disable-software-rasterizer',
-      '--disable-background-timer-throttling',
-      '--js-flags=--max-old-space-size=128',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run'
     ],
   },
 });
